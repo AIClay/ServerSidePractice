de7f2b3dc0d1cae3568fc1bb60c8bdcf5bdebe29
Aaron I. Clay <AIClay@users.noreply.github.com>
2018-04-30 07:28:47 -0400

    Insert Class [PHP]
    [ INCOMPLETE ] 0727 EST 4-30-2018
    -Logic to "insert" records in to database [ testDEV ]

---
+++ ins.php
@@ -0,0 +1,16 @@
+<?php 
+
+	/* INS.php => 04302018.htm / 04302018.js / 04302018.css 
+    
+	PURPOSE:
+	inserts records directly to database [NO VALIDATION! => Brevity ] 
+	
+	VERSION:
+	PHP 7.0.27-0+deb9u1 (cli) (built: Jan  5 2018 13:51:52) ( NTS )
+	Copyright (c) 1997-2017 The PHP Group
+	Zend Engine v3.0.0, Copyright (c) 1998-2017 Zend Technologies
+    with Zend OPcache v7.0.27-0+deb9u1, Copyright (c) 1999-2017, by Zend Technologies
+
+	*/ 
+
+?>

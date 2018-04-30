f067b986effa9d154fc0d548c0b55361460860f6
Aaron I. Clay <AIClay@users.noreply.github.com>
2018-04-30 07:30:13 -0400

    Class to Get records [ PHP ]
    [ INCOMPLETE ]  0729 EST 4-30-2018 
    - logic class to retreive records from database [ testDEV ]

---
+++ get.php
@@ -0,0 +1,15 @@
+<?php
+	
+	/* INS.php => 04302018.htm / 04302018.js / 04302018.css 
+	
+		PURPOSE: simple class to 'GET' stored records on database.
+		
+		VERSION:
+		PHP 7.0.27-0+deb9u1 (cli) (built: Jan  5 2018 13:51:52) ( NTS )
+		Copyright (c) 1997-2017 The PHP Group
+		Zend Engine v3.0.0, Copyright (c) 1998-2017 Zend Technologies
+    	with Zend OPcache v7.0.27-0+deb9u1, Copyright (c) 1999-2017, by Zend Technologies
+		
+	*/
+
+?>

9c7ec0c9c6fde658291dc07497865631eb992606
Aaron I. Clay <AIClay@users.noreply.github.com>
2018-05-01 07:12:38 -0400

    Working *SAMPLE*
    -config issues
    -sample / testing insert *works*
    -Files {"04302018.htm","04302018.css","04302018.js","ins.php","get.php"};
    -STATUS:
           -INCOMPLETE => working

--- ins.php
+++ ins.php
@@ -13,4 +13,44 @@
 
 	*/ 
 
+	
+	$fname = $_POST['FNAME'];
+	$lname = $_POST['LNAME'];
+	$depart = $_POST['DEPART'];
+
+
+
+echo "<em>FNAME: </em>&nbsp;" . $fname . "<br />";
+echo "<em>LNAME: </em>&nbsp;" . $lname . "<br />";
+echo "<em>DEPART: </em>&nbsp;" . $depart . "<br />";
+error_reporting(E_ALL);
+
+$link = mysqli_connect("localhost","DEVTEST","test","testDEV",3306);
+if (!$link)
+{
+	echo mysqli_connect_errno() . PHP_EOL;
+
+}
+/* REFERENCE => INSERT tbl_04302018 (FNAME,LNAME,emp_DEPART) values('testF1','testL1','testD1');*/
+
+$insSTR = "INSERT tbl_04302018 (FNAME,LNAME,emp_DEPART) values('$fname','$lname','$depart');";
+
+echo "==> Inserting....";
+
+$res = mysqli_query($link,$insSTR,MYSQLI_STORE_RESULT);
+
+if (!$res)
+{
+	$message = "invalid insert: " . mysql_error(). "\n";
+	$message = "querySTR: \n" . $insSTR;
+	die($message);
+}
+
+echo "Sucessfully added record." . PHP_EOL;
+
+mysqli_free_result($res);
+
+mysqli_close($link);
+
+
 ?>
